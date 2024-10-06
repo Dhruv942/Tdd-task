038fbf0a6c8d3edc25efbc5bb68fcbc0d12d17b5
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-10-06 11:24:00 +0530

    Add testcases for empty string return 0

--- Stringcalculator.js
+++ Stringcalculator.js
@@ -1,5 +1,4 @@
 function add(numbers) {
-  if (numbers === "") return 0;
   let nums = numbers.split(",").map(Number);
   let sum = 0;
   for (let i = 0; i < nums.length; i++) {
--- stringcalculator.test.js
+++ stringcalculator.test.js
@@ -3,3 +3,7 @@ const { add } = require("./Stringcalculator");
 test("sum of the number returns the Sum", () => {
   expect(add("1,2")).toBe(3);
 });
+
+test("empty string returns 0", () => {
+  expect(add("")).toBe(0);
+});
