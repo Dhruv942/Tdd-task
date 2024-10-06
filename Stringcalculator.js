78cfbe0c78713180a50252214b39e2bf9039464c
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-10-06 18:57:29 +0530

    Implement of custom delimiter returns sum of numbers

--- Stringcalculator.js
+++ Stringcalculator.js
@@ -1,6 +1,14 @@
 function add(numbers) {
   if (numbers == "") return 0;
-  let nums = numbers.replace(/\n/g, ",").split(",").map(Number);
+  let delimiter = ",";
+  if (numbers.startsWith("//")) {
+    delimiter = numbers[2];
+    numbers = numbers.slice(4);
+  }
+
+  numbers = numbers.replace(/\n/g, delimiter);
+
+  let nums = numbers.split(delimiter).map(Number);
   let sum = 0;
   for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
