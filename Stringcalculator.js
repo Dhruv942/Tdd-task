9122881d5aa29bec56fdbb2dfc4e69f319ab217a
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-10-06 11:21:09 +0530

    Implement summing of two numbers

---
+++ Stringcalculator.js
@@ -0,0 +1,14 @@
+function add(numbers) {
+  if (numbers === "") return 0;
+  let nums = numbers.split(",").map(Number);
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= 1000) {
+      sum += nums[i];
+    }
+  }
+
+  return sum;
+}
+
+module.exports = { add };
--- stringcalculator.test.js
+++ stringcalculator.test.js
@@ -1,4 +1,5 @@
 const { add } = require("./Stringcalculator");
+
 test("sum of the number returns the Sum", () => {
   expect(add("1,2")).toBe(3);
 });
