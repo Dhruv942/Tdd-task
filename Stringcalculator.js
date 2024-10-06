42531e0d11173a81a9e04bee7eab832e11a039f2
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-10-06 12:02:09 +0530

    Refactored code

--- Stringcalculator.js
+++ Stringcalculator.js
@@ -3,9 +3,7 @@ function add(numbers) {
   let nums = numbers.replace(/\n/g, ",").split(",").map(Number);
   let sum = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] <= 1000) {
-      sum += nums[i];
-    }
+    sum += nums[i];
   }
 
   return sum;
