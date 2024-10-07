b46813d916f9e2d4507ee4d3d1baf0c4e878fac8
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-10-07 17:01:44 +0530

    Implement of  ingore number bigger than 1000

--- Stringcalculator.js
+++ Stringcalculator.js
@@ -13,7 +13,9 @@ function add(numbers) {
 
   let sum = 0;
   for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
+    if (nums[i] <= 1000) {
+      sum += nums[i];
+    }
   }
 
   return sum;
